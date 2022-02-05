6694e555614893ae8706c3e48e10f040bbc5bba2
MariMJ <99000910+MariMJ@users.noreply.github.com>
2022-02-04 23:29:15 -0400

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.comienzo', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    }) 
+
+    sr.reveal('.carrousel', {
+        duration: 5000,
+        origin: 'top',
+        distance: '-100px',
+    })
+
+    sr.reveal('.primero', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px',
+    })
+
+    sr.reveal('.segundo', {
+        duration: 5000,
+        origin: 'top',
+        distance: '-100px',
+    })
+
+    sr.reveal('.tercero', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px',
+    })
